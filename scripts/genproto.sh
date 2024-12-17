4bac16c9437a50f7f9c2ea885b064be997ddaa9e
yydr <kollug1548@gmail.com>
2024-12-17 17:50:20 +0800

    🔨 update genproto.sh

--- scripts/genproto.sh
+++ scripts/genproto.sh
@@ -26,9 +26,35 @@ function pb_files() {
     echo "${pb_files[@]}"
 }
 
-function get_for_modules() {
+function gen_for_modules() {
     local go_out="internal/common/genproto"
-    if []
+    if [ -d "$go_out" ]; then
+      log_warning "found existing $go_out, cleaning all files under it"
+      run rm -rf $go_out
+    fi
+
+    for dir in $(dirs); do
+      echo "dir=$dir"
+      local service="${dir:0:${#dir}-2}"
+      local pb_file=$"${service}.proto"
+
+      if [ -d "$go_out/$dir" ]; then
+          log_warning "found existing $go_out/$dir, cleaning all files under it"
+          run rm -rf "$go_out"/$dir/*
+      else
+        run mkdir -p "$go_out/$dir"
+      fi
+      log_info "generating code for $service to $go_out/$dir"
+
+      run protoc \
+        -I="usr/local/include" \
+        -I="${API_ROOT}" \
+        "--go_out=${go_out}" --go_opt=paths=source_relative \
+        --go-grpc_opt=require_unimplemented_servers=false \
+        "--go-grpc_out=${go_out}"  --go-grpc_opt=paths=source_relative \
+        "${API_ROOT}/${dir}/$pb_file"
+    done
+    log_success "protoc gen done!"
 }
 
 echo "directories containing protos to be built: $(dirs)"
