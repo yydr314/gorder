fd7eefbde361b7f655feba599d7cbf8ea85773a6
yydr <kollug1548@gmail.com>
2024-12-18 15:16:36 +0800

    🔨 add lint.sh

---
+++ scripts/lint.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source ./scripts/lib.sh
+
+function install_if_not_exist() {
+    TOOL_NAME=$!
+    INSTALL_URL=$2
+    if command -v $TOOL_NAME &> /dev/null
+    then
+      log_callout "$TOOL_NAME is already installed."
+    else
+      log_cmd "$TOOL_NAME is not installed. Installing..."
+      run go install "$INSTALL_URL"
+    fi
+}
+
+install_if_not_exist go-cleanarch github.com/roblaszczak/go-cleanarch@latest
+
+readonly LINT_VERISON="1.54.0"
+NEED_INSTALL=false
+if command -v golangci-lint >/dev/null 2>&1; then
+  CURRENT_VERSION=$(golangci-lint --version | awk '{print $4}' | sed 's/^v//')
+  log_callout "golangci-lint v$CURRENT_VERSION already installed."
+  if [ "$CURRENT_VERSION" == "$LINT_VERISON" ]; then
+      NEED_INSTALL=false
+  else
+    NEED_INSTALL=true
+  fi
+else
+  NEED_INSTALL=true
+fi
+
+if [ "$NEED_INSTALL" == true ]; then
+    run curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.54.0
+fi
+
+run go-cleanarch
+
+log_info "lint modules:"
+log_info "$(modules)"
+
+run goimports -w -l .
+
+while read -r module; do
+  run cd ./internal"$module"
+  run golangci-lint run --config "$ROOT_DIR/.golangci.yaml"
+  run cd -
+done < <(modules)
